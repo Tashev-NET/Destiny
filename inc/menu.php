cd0a9454d605ae0af7ed344dd9ffc375b177e1f4
Metodi Tashev <eraser-group@abv.bg>
2023-02-06 15:58:02 +0200

    code optimization 02 2023

--- inc/menu.php
+++ inc/menu.php
@@ -1,35 +1,29 @@
 <?php
 /*
- * menu.php
- * 
- * Copyright 2022 Metodi Tashev <admin@tashev-net.com>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
+menu.php
+Copyright 2022 Metodi Tashev <admin@tashev-net.com>
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+*/
 ?>
 
 <ul>
-	<li><a href="?p=home">Home</a></li>
-	<li><a href="?p=files">Downloads</a></li>
-	<li><a href="?p=register">Register</a></li>
-	<li><a href="?p=topchars">Top Characters</a></li>
-	<li><a href="?p=topguilds">Top Guilds</a></li>
-	<li><a href="?p=halloffame">Hall of Fame</a></li>
-	<li><a href="?p=statistics">Statistics</a></li>
-	<li><a href="?p=information">Information</a></li>
+  <li><a href="?p=home">Home</a></li>
+  <li><a href="?p=files">Downloads</a></li>
+  <li><a href="?p=register">Register</a></li>
+  <li><a href="?p=topchars">Top Characters</a></li>
+  <li><a href="?p=topguilds">Top Guilds</a></li>
+  <li><a href="?p=halloffame">Hall of Fame</a></li>
+  <li><a href="?p=statistics">Statistics</a></li>
+  <li><a href="?p=information">Information</a></li>
 </ul>
